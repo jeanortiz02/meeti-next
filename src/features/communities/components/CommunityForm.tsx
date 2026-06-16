6427dc0c30fe8749b2d0a48a3535148124698e37
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2026-06-15 22:55:25 -0400

    feat(uploadthing): integrate UploadThing for file uploads and add upload components

--- src/features/communities/components/CommunityForm.tsx
+++ src/features/communities/components/CommunityForm.tsx
@@ -1,10 +1,12 @@
 import {
-  FormError,
-  FormInput,
-  FormLabel,
-  FormTextArea,
+    FormError,
+    FormInput,
+    FormLabel,
+    FormTextArea,
 } from "@/src/shared/components/forms";
+import { UploadDropzone } from "@/src/shared/utils/uploadthing";
 import { useFormContext } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
 import { CommunityInput } from "../schema/CommunitySchema";
 
 export default function CommunityForm() {
@@ -24,6 +26,27 @@ export default function CommunityForm() {
 
       {errors.name && <FormError>{errors.name.message}</FormError>}
 
+      <UploadDropzone endpoint={"meetiUploader"} 
+        className="ut-button:bg-orange-600 hover:ut-button:bg-orange-700"
+        appearance={{
+          button: 'font-black py-3 w-full block h-auto rounded-none after:bg-orange-500 after:h-2 after:top-0',
+          label: 'text-sm text-gray-500 hover:text-gray-700',
+          allowedContent: 'text-sm'
+        }}
+        onClientUploadComplete={(resp) => {{
+          console.log(resp[0].ufsUrl);
+        }}}
+        content={{
+          button: 'Selecciona una imagen',
+          label: 'Arrastra y suelta tu imagen aquí, o haz clic para seleccionar',
+          allowedContent: 'Solo se permiten imágenes (max 1MB)'
+        }}
+        config={{
+          cn: twMerge,
+          mode: 'auto',
+        }}
+      />
+
       <FormLabel htmlFor="description">Descripción Comunidad</FormLabel>
       <FormTextArea
         id="description"
@@ -31,7 +54,9 @@ export default function CommunityForm() {
         {...register("description")}
       />
 
-      {errors.description && <FormError>{errors.description.message}</FormError>}
+      {errors.description && (
+        <FormError>{errors.description.message}</FormError>
+      )}
     </>
   );
 }
